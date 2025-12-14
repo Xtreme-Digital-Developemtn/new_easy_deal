799c9a4b6689b0dc626316be5451e9f8e85a530d
mostafa bahr <mustafa.bahr12323@gmail.com>
2025-12-14 12:32:43 +0200

    edit

--- lib/features/broker_features/broker_developers/presentation/views/broker_developers_view.dart
+++ lib/features/broker_features/broker_developers/presentation/views/broker_developers_view.dart
@@ -1,6 +1,5 @@
 import 'package:easy_deal/features/broker_features/broker_developers/presentation/views/widgets/developers_table_data.dart';
 import 'package:easy_deal/main_imports.dart';
-import 'package:easy_localization/easy_localization.dart';
 
 class BrokerDevelopersView extends StatelessWidget {
   const BrokerDevelopersView({super.key});
@@ -8,7 +7,7 @@ class BrokerDevelopersView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: GlobalAppBar(title: LangKeys.developers.tr()),
+      appBar: GlobalAppBar(title: LangKeys.developers),
       body: Column(
         children: [
           Expanded(child: DevelopersTableData()),
