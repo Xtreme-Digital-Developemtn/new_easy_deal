96ad5bee0f82e0c9a074530d573f92817fae2f25
Mostafa Ahmed <79056512+mostafaahmedbahr@users.noreply.github.com>
2026-06-23 14:38:30 +0200

    do the otp done and the send code button

--- lib/features/register/presentation/views/widgets/password_widgets/create_password_buttons.dart
+++ lib/features/register/presentation/views/widgets/password_widgets/create_password_buttons.dart
@@ -1,4 +1,3 @@
-import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:easy_deal/core/utils/toast/toast.dart';
 import 'package:easy_deal/features/register/presentation/view_model/register_cubit.dart';
 import 'package:easy_deal/features/register/presentation/view_model/register_states.dart';
@@ -11,7 +10,31 @@ class CreatePasswordButtons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<RegisterCubit, RegisterStates>(
+    return BlocConsumer<RegisterCubit, RegisterStates>(
+      listener: (context, state) {
+        if (state is SendOtpSuccessState) {
+          var registerCubit = context.read<RegisterCubit>();
+          Toast.showSuccessToast(
+              msg: state.sendOtpModel.message.toString(), context: context);
+          context.pushNamed(
+            Routes.otpView,
+            arguments: {
+              "isMobile": true,
+              "contact": registerCubit.phoneNumber,
+              "selectIndex": registerCubit.selectBrokerIndex,
+              "gender": registerCubit.gender,
+              "role": registerCubit.role,
+              "name": registerCubit.nameCon.text,
+              "email": registerCubit.emailCon.text,
+              "password": registerCubit.passwordCon.text,
+              "confirmPassword": registerCubit.confirmPasswordCon.text,
+              "phone": registerCubit.phoneCon.text,
+            },
+          );
+        } else if (state is SendOtpErrorState) {
+          Toast.showErrorToast(msg: state.error.toString(), context: context);
+        }
+      },
       builder: (context, state) {
         var registerCubit = context.read<RegisterCubit>();
         return Column(
@@ -29,80 +52,23 @@ class CreatePasswordButtons extends StatelessWidget {
                   child: ValueListenableBuilder<bool>(
                     valueListenable: registerCubit.isFormValid,
                     builder: (_, isValid, __) {
+                      if (state is SendOtpLoadingState) {
+                        return const CustomLoading();
+                      }
                       return CustomButton(
                         onPressed: isValid
                             ? () {
-                          registerCubit.changeStepperIndex(4);
-                          print(registerCubit.phoneCon.text);
-                          print(registerCubit.emailCon.text);
-                          print(registerCubit.nameCon.text);
-                          print(registerCubit.passwordCon.text);
-                          print("wwwwwwwwwwwwwwwwwwwwwsssssssssssssssssdddddddd");
-                        }
+                                registerCubit.sendOtp(
+                                    phone: registerCubit.phoneCon.text);
+                              }
                             : null,
-                        text: LangKeys.next.tr(),
+                        text: LangKeys.sendCode.tr(),
                       );
                     },
                   ),
                 ),
-
               ],
             ),
-            Gap(12.h),
-            if (registerCubit.activeStep == 4)
-              BlocConsumer<RegisterCubit , RegisterStates>(
-                listener:(context,state){
-                  if(state is SendOtpSuccessState){
-                    Toast.showSuccessToast(msg: state.sendOtpModel.message.toString(), context: context);
-                    context.pushNamed(
-                      Routes.otpView,
-                      arguments: {
-                        "isMobile": true,
-                        "contact": registerCubit.phoneNumber,
-                        "selectIndex": registerCubit.selectBrokerIndex,
-                        "gender": registerCubit.gender,
-                        "role": registerCubit.role,
-                        "name": registerCubit.nameCon.text,
-                        "email": registerCubit.emailCon.text,
-                        "password": registerCubit.passwordCon.text,
-                        "confirmPassword": registerCubit.confirmPasswordCon.text,
-                        "phone": registerCubit.phoneCon.text,
-
-                      },
-                    );
-                  }
-                  else if (state is SendOtpErrorState){
-                    Toast.showErrorToast(msg: state.error.toString(), context: context);
-                  }
-                } ,
-                builder: (context,state){
-                 return   ConditionalBuilder(
-                   condition: state is ! SendOtpLoadingState,
-                   fallback: (context)=>CustomLoading(),
-                  builder: (context){
-                     return CustomButton(
-                       onPressed: () {
-                         if (formKey.currentState!.validate()) {
-                           registerCubit.sendOtp(phone: registerCubit.phoneCon.text);
-                           // registerCubit.register(
-                           //   fullName: registerCubit.nameCon.text,
-                           //   phone: registerCubit.phoneCon.text,
-                           //   password:registerCubit.passwordCon.text,
-                           //   passwordConfirmation: registerCubit.confirmPasswordCon.text,
-                           //   gender: registerCubit.gender.toString(),
-                           //   email: registerCubit.emailCon.text,
-                           //   image: null,
-                           // );
-
-                         }
-                       },
-                       text: LangKeys.sendCode.tr(),
-                     );
-                  },
-                 );
-                } ,
-
-              ),
           ],
         );
       },
