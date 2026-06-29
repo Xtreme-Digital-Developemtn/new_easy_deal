fd2ccc25e44c7f324e11ae5f900e986d1c85b688
Mostafa Ahmed <79056512+mostafaahmedbahr@users.noreply.github.com>
2026-06-29 09:47:34 +0200

    remove the previous button in the first step of register

--- lib/features/register/presentation/views/widgets/account_type_widgets/selection_account_type_button.dart
+++ lib/features/register/presentation/views/widgets/account_type_widgets/selection_account_type_button.dart
@@ -12,24 +12,11 @@ class SelectionAccountTypeButton extends StatelessWidget {
     return BlocBuilder<RegisterCubit , RegisterStates>(
       builder: (context,state){
         var registerCubit = context.read<RegisterCubit>();
-        return Row(
-          children: [
-            Expanded(
-              child: CustomButton(
-                onPressed: () => context.pop(),
-                text: LangKeys.past.tr(),
-              ),
-            ),
-            Gap(12.w),
-            Expanded(
-              child: CustomButton(
-                onPressed: registerCubit.selectTypeIndex == 0 ? null : () {
-                  registerCubit.changeStepperIndex(1);
-                },
-                text: LangKeys.next.tr(),
-              ),
-            ),
-          ],
+        return CustomButton(
+          onPressed: registerCubit.selectTypeIndex == 0 ? null : () {
+            registerCubit.changeStepperIndex(1);
+          },
+          text: LangKeys.next.tr(),
         );
       },
 
