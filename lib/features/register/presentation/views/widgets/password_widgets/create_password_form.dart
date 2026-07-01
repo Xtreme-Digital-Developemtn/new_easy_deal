7f237969ed163bfbe8970f4b5569b8761dd04301
Mostafa Ahmed <79056512+mostafaahmedbahr@users.noreply.github.com>
2026-07-01 23:20:44 +0300

    add new validation in password

--- lib/features/register/presentation/views/widgets/password_widgets/create_password_form.dart
+++ lib/features/register/presentation/views/widgets/password_widgets/create_password_form.dart
@@ -1,11 +1,31 @@
 import 'package:easy_deal/main_imports.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter_pw_validator/flutter_pw_validator.dart';
 import '../../../view_model/register_cubit.dart';
 
-class CreatePasswordForm extends StatelessWidget {
+class CreatePasswordForm extends StatefulWidget {
   const CreatePasswordForm({super.key, required this.formKey});
   final GlobalKey<FormState> formKey;
 
+  @override
+  State<CreatePasswordForm> createState() => _CreatePasswordFormState();
+}
+
+class _CreatePasswordFormState extends State<CreatePasswordForm> {
+  late TextEditingController _passwordController;
+  bool isPasswordValid = false;
+  @override
+  void initState() {
+    super.initState();
+    _passwordController = TextEditingController();
+
+  }
+
+  @override
+  void dispose() {
+    _passwordController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     final registerCubit = context.read<RegisterCubit>();
@@ -28,13 +48,34 @@ class CreatePasswordForm extends StatelessWidget {
           controller: registerCubit.passwordCon,
           hintText: LangKeys.password.tr(),
           obscureText: !registerCubit.isPasswordVisible,
-          validator: (v) =>
-              AppValidators.passwordValidator(registerCubit.passwordCon.text),
+          // validator: (v) =>
+          //     AppValidators.passwordValidator(registerCubit.passwordCon.text),
           onChanged: (_) {
-            registerCubit.onPasswordInteraction(formKey);
+            registerCubit.onPasswordInteraction(widget.formKey);
           },
         ),
 
+        Gap(6.h),
+        FlutterPwValidator(
+          controller: registerCubit.passwordCon,
+          minLength: 8,
+          uppercaseCharCount: 1,
+          lowercaseCharCount: 1,
+          numericCharCount: 1,
+          specialCharCount: 1,
+          width: 250.w,
+          height: 140.h,
+          onSuccess: () {
+            setState(() {
+              isPasswordValid = true;
+            });
+          },
+          onFail: () {
+            setState(() {
+              isPasswordValid = false;
+            });
+          },
+        ),
         Gap(20.h),
 
         /// Confirm Password
@@ -58,7 +99,7 @@ class CreatePasswordForm extends StatelessWidget {
                 password: registerCubit.passwordCon.text,
               ),
           onChanged: (_) {
-            registerCubit.onPasswordInteraction(formKey);
+            registerCubit.onPasswordInteraction(widget.formKey);
           },
         ),
       ],
